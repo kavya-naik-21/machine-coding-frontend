8f378c3b20ef0abac79a349f77b8a16386e08bef
Kavya Naik <kavyanaik2112@gmail.com>
2025-03-16 14:37:07 +0530

    update folder

--- 03-file-explorer/file-explorer/src/App.jsx
+++ 03-file-explorer/file-explorer/src/App.jsx
@@ -1,13 +1,35 @@
 import React, { useState } from "react";
 import Folder from "./components/Folder";
 import data from "./data/folderData.json";
-import './App.css'
+import "./App.css";
 
 const App = () => {
   const [folderData, setFolderData] = useState(data);
+
+  const createFolder = (folderId) => {
+    const updateFolderStructure = (tree) => {
+      if (tree.isFolder) {
+        if (tree.id === folderId) {
+          tree.children.unshift({
+            id: Date.now().toString(),
+            name: "new folder",
+            isFolder: true,
+            children: [],
+          });
+        } else {
+          tree.children.map((child) => {
+            return updateFolderStructure(child);
+          });
+        }
+      }
+      return tree;
+    };
+
+    setFolderData((prev) => updateFolderStructure(prev));
+  };
   return (
     <div className="app">
-      <Folder list={folderData} />
+      <Folder list={folderData} createFolder={createFolder} />
     </div>
   );
 };
--- 03-file-explorer/file-explorer/src/components/Folder.jsx
+++ 03-file-explorer/file-explorer/src/components/Folder.jsx
@@ -3,23 +3,39 @@ import { FaFolder } from "react-icons/fa";
 import { FaRegFileAlt } from "react-icons/fa";
 import "../App.css";
 
-const Folder = ({ list }) => {
+const Folder = ({ list, createFolder }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   if (list.isFolder) {
     return (
       <div className="item">
-        <div
-          className="item-box"
-          onClick={() => {
-            setIsExpanded(!isExpanded);
-          }}
-        >
-          <FaFolder />
-          {list.name}
+        <div className="item-box">
+          <div
+            className="folder-name"
+            onClick={() => {
+              setIsExpanded(!isExpanded);
+            }}
+          >
+            <FaFolder />
+            {list.name}
+          </div>
+          <div className="control-btns">
+            <div
+              className="add-folder"
+              onClick={() => {
+                createFolder(list.id);
+                setIsExpanded(true);
+              }}
+            >
+              Folder +
+            </div>
+            <div className="add-file"> File + </div>
+          </div>
         </div>
         {isExpanded &&
           list.children.map((item) => {
-            return <Folder list={item} key={item.id} />;
+            return (
+              <Folder list={item} key={item.id} createFolder={createFolder} />
+            );
           })}
       </div>
     );
@@ -27,7 +43,6 @@ const Folder = ({ list }) => {
     return (
       <div className="item">
         <div className="item-box">
-          {" "}
           <FaRegFileAlt />
           {list.name}
         </div>
