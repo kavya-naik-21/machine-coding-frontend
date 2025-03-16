fa4d56aea5c2cc6d469b9ae8e43077fe9e9f48e9
Kavya Naik <kavyanaik2112@gmail.com>
2025-03-16 11:03:02 +0530

    expand / collapse folders

--- 03-file-explorer/file-explorer/src/components/Folder.jsx
+++ 03-file-explorer/file-explorer/src/components/Folder.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaFolder } from "react-icons/fa";
 import { FaRegFileAlt } from "react-icons/fa";
 import "../App.css";
 
 const Folder = ({ list }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
   if (list.isFolder) {
     return (
       <div className="item">
-        <FaFolder />
-        {list.name}
-        {list.children.map((item) => {
-          return <Folder list={item} key={item.id} />;
-        })}
+        <div
+          className="item-box"
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+          }}
+        >
+          <FaFolder />
+          {list.name}
+        </div>
+        {isExpanded &&
+          list.children.map((item) => {
+            return <Folder list={item} key={item.id} />;
+          })}
       </div>
     );
   } else {
     return (
       <div className="item">
-        <FaRegFileAlt />
-        {list.name}
+        <div className="item-box">
+          {" "}
+          <FaRegFileAlt />
+          {list.name}
+        </div>
       </div>
     );
   }
